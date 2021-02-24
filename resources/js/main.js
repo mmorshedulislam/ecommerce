1ea29a684e0d6d1e29ea23b544b57a54fd9eabfd
Morshed <morshed952640@gmail.com>
2021-02-24 20:21:50 +0600

    First edit

---
+++ resources/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	$(".arrivals-heading h2").click(function(){
+		$(this).hide();
+	})
+
+
+
+	$('.menu-icon').click(function(){
+		$(this).hide();
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
